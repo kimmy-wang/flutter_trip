2d73b57edf0e03a469f57a6e31a82591c2f327b9
upcwangying <upcwangying@126.com>
2019-07-14 19:14:37 +0800

    feat: WebView组件开发

---
+++ lib/widgets/webview.dart
@@ -0,0 +1,156 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+const CATCH_URLS = ['m.ctrip.com/', 'm.ctrip.com/html5/', 'm.ctrip.com/html5'];
+
+class WebView extends StatefulWidget {
+  final String url;
+  final String statusBarColor;
+  final String title;
+  final bool hideAppBar;
+  final bool backForbid;
+
+  WebView(
+      {@required this.url,
+      this.statusBarColor,
+      this.title,
+      this.hideAppBar,
+      this.backForbid = false})
+      : assert(url != null, 'url must not be null!');
+
+  @override
+  _WebViewState createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebView> {
+  final flutterWebviewPlugin = FlutterWebviewPlugin();
+  StreamSubscription<String> _onUrlChanged;
+  StreamSubscription<WebViewStateChanged> _onStateChanged;
+  StreamSubscription<WebViewHttpError> _onHttpError;
+  bool exiting = false;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterWebviewPlugin.close();
+    _onUrlChanged = flutterWebviewPlugin.onUrlChanged.listen((String url) {});
+    _onStateChanged =
+        flutterWebviewPlugin.onStateChanged.listen((WebViewStateChanged state) {
+      switch (state.type) {
+        case WebViewState.startLoad:
+          if (_isToMain(state.url) && !exiting) {
+            if (widget.backForbid) {
+              flutterWebviewPlugin.launch(widget.url);
+            } else {
+              Navigator.pop(context);
+              exiting = true;
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    });
+    _onHttpError =
+        flutterWebviewPlugin.onHttpError.listen((WebViewHttpError error) {
+      print(error);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_onUrlChanged != null) {
+      _onUrlChanged.cancel();
+    }
+    if (_onStateChanged != null) {
+      _onStateChanged.cancel();
+    }
+    if (_onHttpError != null) {
+      _onHttpError.cancel();
+    }
+    flutterWebviewPlugin.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String statusBarColorStr = widget.statusBarColor ?? 'ffffff';
+    Color backButtonColor =
+        statusBarColorStr == 'ffffff' ? Colors.black : Colors.white;
+
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          _appBar(
+              Color(int.parse('0xff' + statusBarColorStr)), backButtonColor),
+          Expanded(
+            child: WebviewScaffold(
+              url: widget.url,
+              withZoom: true,
+              withLocalStorage: true,
+              hidden: true,
+              initialChild: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Text('Loading...'),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _appBar(Color backgroundColor, Color backButtonColor) {
+    if (widget.hideAppBar ?? false) {
+      return Container(
+        color: backgroundColor,
+        height: 30,
+      );
+    }
+
+    return Container(
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: Stack(
+          children: <Widget>[
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: EdgeInsets.only(left: 10),
+                child: Icon(
+                  Icons.close,
+                  color: backButtonColor,
+                  size: 26,
+                ),
+              ),
+            ),
+            Positioned(
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Text(
+                    widget.title ?? '',
+                    style: TextStyle(color: backButtonColor, fontSize: 20),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  _isToMain(String url) {
+    bool contains = false;
+    for (final value in CATCH_URLS) {
+      if (url?.endsWith(value) ?? false) {
+        contains = true;
+        break;
+      }
+    }
+    return contains;
+  }
+}
