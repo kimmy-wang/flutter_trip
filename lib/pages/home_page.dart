d25f9424e832f6e8d38fd18e24b093bf66cb2531
upcwangying <upcwangying@126.com>
2019-07-21 20:53:44 +0800

    feat: Search组件开发

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,10 +10,12 @@ import 'package:flutter_trip/widgets/grid_nav.dart';
 import 'package:flutter_trip/widgets/loading.dart';
 import 'package:flutter_trip/widgets/local_nav.dart';
 import 'package:flutter_trip/widgets/sales_box.dart';
+import 'package:flutter_trip/widgets/search.dart';
 import 'package:flutter_trip/widgets/sub_nav.dart';
 import 'package:flutter_trip/widgets/webview.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
+const SEARCH_BAR_DEFAULT_TEXT = "网红打卡地 景点 酒店 美食";
 
 class HomePage extends StatefulWidget {
   @override
@@ -137,19 +139,55 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget get _appBar {
-    return Opacity(
+  /**
+   * Opacity(
       opacity: appBarAlpha,
       child: Container(
-        height: 80,
-        decoration: BoxDecoration(color: Colors.white),
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text('首页'),
+      height: 80,
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+      child: Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Text('首页'),
+      ),
+      ),
+      ),
+      )
+   */
+  Widget get _appBar {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0x66000000), Colors.transparent],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter),
+          ),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+            height: 80.0,
+            decoration: BoxDecoration(
+                color:
+                    Color.fromARGB((appBarAlpha * 255).toInt(), 255, 255, 255)),
+            child: Search(
+              searchBarType: appBarAlpha > 0.2
+                  ? SearchBarType.homeLight
+                  : SearchBarType.home,
+              inputBoxClick: _jumpToSearch,
+              speakClick: _jumpToSpeak,
+              defaultText: SEARCH_BAR_DEFAULT_TEXT,
+              leftButtonClick: () {},
+            ),
           ),
         ),
-      ),
+        Container(
+          height: appBarAlpha > 0.2 ? 0.5 : 0,
+          decoration: BoxDecoration(
+            boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 0.5)]
+          ),
+        )
+      ],
     );
   }
 
@@ -161,13 +199,15 @@ class _HomePageState extends State<HomePage> {
         autoplay: true,
         itemBuilder: (BuildContext context, int index) {
           return GestureDetector(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => WebView(
-                    url: bannerList[index].url,
-                    title: bannerList[index].title,
-                    hideAppBar: bannerList[index].hideAppBar,
-                  )));
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => WebView(
+                            url: bannerList[index].url,
+                            title: bannerList[index].title,
+                            hideAppBar: bannerList[index].hideAppBar,
+                          )));
             },
             child: Image.network(
               bannerList[index].icon,
@@ -179,4 +219,8 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  void _jumpToSearch() {}
+
+  void _jumpToSpeak() {}
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trip/widgets/search.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -10,9 +11,24 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('搜索'),
+      appBar: AppBar(),
+      body: Column(
+        children: <Widget>[
+          Search(
+            hideLeft: true,
+            defaultText: '搜索',
+            hint: '123',
+            leftButtonClick: (){
+              Navigator.pop(context);
+            },
+            onChanged: _onTextChange,
+          )
+        ],
       ),
     );
+  }
+
+  void _onTextChange(String text) {
+
   }
 }
